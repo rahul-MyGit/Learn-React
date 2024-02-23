b9d63a760ddfa1e7e77e0fbe0b83d27f98a25010
Rahul <rahulhero93111@gmail.com>
2024-02-23 13:28:18 +0530

    props in component

--- 03Tailwindprops/src/App.jsx
+++ 03Tailwindprops/src/App.jsx
@@ -31,15 +31,23 @@
 
 // export default App
 
-
+import Card from './components/Card'
 import './App.css'
 
 function App() {
 
+  // let myObj = {
+  //   username : "Rahul",
+  //   age : 21
+  // }
+  // let newArr = [1,2,3,4]
+
   return (
     <>
        <h1 className='bg-green-400 text-black p-4 
-       rounded-xl'>Tailwind test</h1>
+       rounded-xl mb-4'>Tailwind test</h1>
+       <Card username="Chai or Code" btnText="click me"/>
+       <Card username= "Rahul" />
     </>
   )
 }
