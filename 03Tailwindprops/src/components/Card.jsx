c2d56bdde8d886250b4f217df23ce07915b4cc34
Rahul <rahulhero93111@gmail.com>
2024-02-23 13:32:44 +0530

    props in component

--- 03Tailwindprops/src/components/Card.jsx
+++ 03Tailwindprops/src/components/Card.jsx
@@ -3,7 +3,7 @@
 
 function Card(props) {
   return (
-    <div className="relative h-[400px] w-[300px] rounded-md">
+    <div className="relative h-[400px] w-[300px] rounded-md mb-2">
       <img
         src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
         alt="AirMax Pro"
